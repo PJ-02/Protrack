4b48f8a96754292b95747e6f0045595cd833d04a
Prathmesh Jain <prathmeshjain02@gmail.com>
2023-01-14 12:50:22 -0800

    Initial Commit

---
+++ Protrack/ProtrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtrackApp.swift
+//  Protrack
+//
+//  Created by Prathmesh Jain on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ProtrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
