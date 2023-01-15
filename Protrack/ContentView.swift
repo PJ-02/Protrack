e80170b47d85d786320afc050bc575efb4eaaa9a
Prathmesh Jain <prathmeshjain02@gmail.com>
2023-01-15 11:57:46 -0800

    Contentview

--- Protrack/ContentView.swift
+++ Protrack/ContentView.swift
@@ -8,96 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Text("Protrack")
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundColor(Color.green)
-            HStack{
-                Image(systemName: "apple.logo")
-                    .padding(/*@START_MENU_TOKEN@*/[.top, .leading, .bottom]/*@END_MENU_TOKEN@*/)
-                    .imageScale(.large)
-                    .foregroundColor(.orange)
-                Image(systemName: "plus")
-                    .padding(.vertical)
-                    .imageScale(.large)
-                    .foregroundColor(.orange)
-                Image(systemName: "timer")
-                    .padding(/*@START_MENU_TOKEN@*/[.top, .bottom, .trailing]/*@END_MENU_TOKEN@*/)
-                    .imageScale(.large)
-                    .foregroundColor(.orange)
-            }
-            HStack{
-                Text("Enter Item Name:")
-                    .padding(.leading)
-                TextField("Item Name", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
-            }
-            List {
-                Button(action: {
-                    print("button pressed")
-                }) {
-                    Image("apple")
-                        .resizable()
-                        .frame(width: 85.0, height: 85.0)
-                }
-                Button(action: {
-                    print("button pressed")
-                }) {
-                    Image("orange")
-                        .resizable()
-                        .frame(width: 85.0, height: 85.0)
-                }
-                Button(action: {
-                    print("button pressed")
-                }) {
-                    Image("banana")
-                        .resizable()
-                        .frame(width: 85.0, height: 85.0)
-                }
-                Button(action: {
-                    print("button pressed")
-                }) {
-                    Image("grape")
-                        .resizable()
-                        .frame(width: 85.0, height: 85.0)
-                }
-                Button(action: {
-                    print("button pressed")
-                }) {
-                    Image("onion")
-                        .resizable()
-                        .frame(width: 85.0, height: 85.0)
-                }
-                Button(action: {
-                    print("button pressed")
-                }) {
-                    Image("cucumber")
-                        .resizable()
-                        .frame(width: 85.0, height: 85.0)
-                }
-                Button(action: {
-                    print("button pressed")
-                }) {
-                    Image("avacado")
-                        .resizable()
-                        .frame(width: 85.0, height: 85.0)
-                }
-                Button(action: {
-                    print("button pressed")
-                }) {
-                    Image("brocolli")
-                        .resizable()
-                        .frame(width: 85.0, height: 85.0)
-                }
-            }
+    //    @State var purchasedDate = Date.now
+        var body: some View {
+                ZStack {
+                        //            Image("background")
+                        //                .imageScale(.large)
+            //            Image("background")
+            //                .resizable()
+            //                .frame(width: 400)
+            //                .opacity(0.35)
+                        //            Color(red:230, green:248, blue: 227)
+                        VStack {
+                                Spacer()
+                                Image("ICON")
+                                    .resizable()
+                                    .frame(width:100, height:100)
+                
+                                Text("Welcome to ProTrack")
+                                    .font(.custom("Thonburi", size: 30))
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color(hue: 0.297, saturation: 0.192, brightness: 0.642))
+                //                    .background(Color.gray.edgesIgnoringSafeArea(.all))
+                                Spacer()
+                
+                //                DatePicker(selection: $purchasedDate, label: { Text("Date Purchased:") })
+                            }
+                        .padding()
         }
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
