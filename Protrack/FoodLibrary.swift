8fe88a78c80cdb85b6163f5dfd79e22667831dbc
mmsarkar <medhamsarkar@gmail.com>
2023-01-14 16:44:03 -0800

    I added a bunch more buttons
    All the fruits are added and properly spaced, it's not showing up so I'm updating it in case it decides to die on me.

--- Protrack/FoodLibrary.swift
+++ Protrack/FoodLibrary.swift
@@ -10,13 +10,14 @@ import SwiftUI
 
 struct FoodLibrary: View {
     var body: some View {
-        VStack (alignment: .leading){
+        VStack {
             Text("!!  Food Library  !!")
                 .font(.largeTitle)
                 .fontWeight(.semibold)
                 .foregroundColor(Color.black)
                 .multilineTextAlignment(.center)
             HStack {
+                Spacer()
                 Button(action: {
                     print("button pressed")
                 }) {
@@ -24,6 +25,8 @@ struct FoodLibrary: View {
                         .resizable()
                         .frame(width: 40.0, height: 40.0)
                 }
+                Spacer(minLength: 2)
+                
                 Button(action: {
                     print("button pressed")
                 }) {
@@ -31,6 +34,8 @@ struct FoodLibrary: View {
                         .resizable()
                         .frame(width: 40.0, height: 40.0)
                 }
+                Spacer(minLength: 2)
+                
                 Button(action: {
                     print("button pressed")
                 }) {
@@ -38,6 +43,8 @@ struct FoodLibrary: View {
                         .resizable()
                         .frame(width: 40.0, height: 40.0)
                 }
+                Spacer(minLength: 2)
+                
                 Button(action: {
                     print("button pressed")
                 }) {
@@ -45,15 +52,76 @@ struct FoodLibrary: View {
                         .resizable()
                         .frame(width: 40.0, height: 40.0)
                 }
+                Spacer()
             }
             HStack {
+                Spacer()
+                Button(action: {
+                    print("button pressed")
+                }) {
+                    Image("blueberry")
+                        .resizable()
+                        .frame(width: 40.0, height: 40.0)
+                }
+                Spacer()
+                
+                Button(action: {
+                    print("button pressed")
+                }) {
+                    Image("strawberry")
+                        .resizable()
+                        .frame(width: 40.0, height: 40.0)
+                }
+                Spacer()
+                
+                Button(action: {
+                    print("button pressed")
+                }) {
+                    Image("cherries")
+                        .resizable()
+                        .frame(width: 40.0, height: 40.0)
+                }
+                Spacer()
+                
+                Button(action: {
+                    print("button pressed")
+                }) {
+                    Image("peach")
+                        .resizable()
+                        .frame(width: 40.0, height: 40.0)
+                }
+                Spacer()
+                
+            }
+            HStack {
+                Button(action: {
+                    print("button pressed")
+                }) {
+                    Image("pear")
+                        .resizable()
+                        .frame(width: 40.0, height: 40.0)
+                }
+                Spacer()
+                
                 Button(action: {
                     print("button pressed")
                 }) {
                     Image("apple")
                         .resizable()
                         .frame(width: 40.0, height: 40.0)
-            }
+                }
+                Spacer()
+                
+                Button(action: {
+                    print("button pressed")
+                }) {
+                    Image("apple")
+                        .resizable()
+                        .frame(width: 40.0, height: 40.0)
+                }
+                Spacer()
+                
+                
             }
         }
         
